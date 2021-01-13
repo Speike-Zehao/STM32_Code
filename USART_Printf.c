b0fa5d9a4bace5eb8170aabe905a7f7ad9718d5a
SimilarTriangles <36286155+Speike-Zehao@users.noreply.github.com>
2021-01-13 19:40:21 +0800

    Update USART_Printf.c

--- USART_Printf.c
+++ USART_Printf.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 
-///* USER CODE BEGIN 4 */
-//int fputc(int ch,FILE *f)
-//{
-//    HAL_UART_Transmit(&huart1, (uint8_t*)&ch,1,HAL_MAX_DELAY);
-//    return ch;
-//}
-///* USER CODE END 4 */
-///Editing all of above to enable your USART put messages to your computer.
-///(Here is usart1,STM32CubeMX give its structure 'huart1')
+/* USER CODE BEGIN 4 */
+int fputc(int ch,FILE *f)
+{
+    HAL_UART_Transmit(&huart1, (uint8_t*)&ch,1,HAL_MAX_DELAY);
+    return ch;
+}
+/* USER CODE END 4 */
+///Editing all of above to enable your USART put messages to computer.
+///(Here is usart1,STM32CubeMX give its structure name of 'huart1')
 
