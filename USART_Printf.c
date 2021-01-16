f950057547989c2b7de0cffb3fc22fb36931cc99
SimilarTriangles <36286155+Speike-Zehao@users.noreply.github.com>
2021-01-16 19:05:09 +0800

    Update USART_Printf.c

--- USART_Printf.c
+++ USART_Printf.c
@@ -10,3 +10,21 @@ int fputc(int ch,FILE *f)
 ///Editing all of above to enable your USART put messages to computer.
 ///(Here is usart1,STM32CubeMX give its structure name of 'huart1')
 
+
+
+//串口初始化
+static void MX_USART1_UART_Init(void)
+{
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
