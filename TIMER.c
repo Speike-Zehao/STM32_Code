a16c8ac8d1c1bd6752e9d75db6e614590413f8bb
SimilarTriangles <36286155+Speike-Zehao@users.noreply.github.com>
2021-01-16 17:17:18 +0800

    Update TIMER.c

--- TIMER.c
+++ TIMER.c
@@ -3,3 +3,19 @@ HAL_TIM_Base_Start_IT(&htim10); //初始化定时器，使能TIMER10的更新中
 HAL_TIM_Base_Start(&htim10); //初始化定时器，但不使能更新中断。
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){}
 //定时器中断服务函数
+
+//以下是单通道基本定时器的初始化函数
+static void MX_TIM10_Init(void)
+{
+  htim10.Instance = TIM10;
+  htim10.Init.Prescaler = 16799;
+  htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim10.Init.Period = 9999;
+  htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
