dd8d94f9fb43b3cb17de97197250572ccfd74882
SimilarTriangles <36286155+Speike-Zehao@users.noreply.github.com>
2021-01-16 22:39:58 +0800

    Update breathLED.c

--- Test&NotFinished/breathLED.c
+++ Test&NotFinished/breathLED.c
@@ -59,6 +59,7 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN 0 */
 int PWM(short pwmValues);
 int test(int num);
+void LED();
 /* USER CODE END 0 */
 
 /**
@@ -67,59 +68,47 @@ int test(int num);
   */
 int main(void)
 {
-    /* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
+   
+	 
+  /* USER CODE END 1 */
 
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* USER CODE END 1 */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* USER CODE BEGIN Init */
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* USER CODE END Init */
 
-    /* USER CODE BEGIN Init */
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* USER CODE END Init */
+  /* USER CODE BEGIN SysInit */
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* USER CODE END SysInit */
 
-    /* USER CODE BEGIN SysInit */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1);
+  /* USER CODE END 2 */
 
-    /* USER CODE END SysInit */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		LED();
+		HAL_Delay(1000);
+	
+    /* USER CODE END WHILE */
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_TIM2_Init();
-    MX_USART1_UART_Init();
-    /* USER CODE BEGIN 2 */
-    HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1);
-    /* USER CODE END 2 */
-
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
-    while (1)
-    {
-        while (pwmVal< 199)
-        {
-            pwmVal++;
-            __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, pwmVal);
-
-            HAL_Delay(10);
-        }
-        while (pwmVal)
-        {
-            pwmVal--;
-            __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, pwmVal);
-            HAL_Delay(10);
-        }
-        HAL_Delay(1000);
-
-        /* USER CODE END WHILE */
-
-        /* USER CODE BEGIN 3 */
-    }
-    /* USER CODE END 3 */
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
 /**
@@ -128,68 +117,83 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Configure the main internal regulator output voltage
-    */
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 4;
-    RCC_OscInitStruct.PLL.PLLN = 168;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 4;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-    {
-        Error_Handler();
-    }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
 
 int PWM(short pwmValues){
-    if(pwmValues<100){
-        printf("%d\r\n",pwmValues);
-        TIM2->CCR1 = pwmValues;
-        //	__HAL_TIM_SetCompare(&htim2,TIM_CHANNEL_1,pwmValues);
-        HAL_Delay(50);
-        PWM(pwmValues+1);
-    }
-    printf("%d\r\n",pwmValues);
-    TIM2->CCR1 = pwmValues;
-    //	__HAL_TIM_SetCompare(&htim2,TIM_CHANNEL_1,pwmValues);
-    HAL_Delay(50);
+	if(pwmValues<100){
+		printf("%d\r\n",pwmValues);
+		TIM2->CCR1 = pwmValues;
+	//	__HAL_TIM_SetCompare(&htim2,TIM_CHANNEL_1,pwmValues);
+		HAL_Delay(50);
+		PWM(pwmValues+1);
+	}
+	printf("%d\r\n",pwmValues);
+	TIM2->CCR1 = pwmValues;
+	//	__HAL_TIM_SetCompare(&htim2,TIM_CHANNEL_1,pwmValues);
+	HAL_Delay(50);
 }
 
 int test(int num){
-    if(num < 100){
-        printf("%d\r\n",num);
-        HAL_Delay(50);
-        test(num+1);
-    }
-    printf("%d\r\n",num);
-    HAL_Delay(50);
+	if(num < 100){
+		printf("%d\r\n",num);
+		HAL_Delay(50);
+		test(num+1);
+	}
+	printf("%d\r\n",num);
+	HAL_Delay(50);
 }
 
+void LED(){
+	while (pwmVal< 199)
+	  {
+		  pwmVal++;
+		  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, pwmVal);   
+
+		  HAL_Delay(10);
+	  }
+	  while (pwmVal)
+	  {
+		  pwmVal--;
+		  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, pwmVal);   
+		  HAL_Delay(10);
+	  }
+}
 int fputc(int ch,FILE *f)
 {
     HAL_UART_Transmit(&huart1, (uint8_t*)&ch,1,HAL_MAX_DELAY);
@@ -204,10 +208,10 @@ int fputc(int ch,FILE *f)
   */
 void Error_Handler(void)
 {
-    /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state */
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
 
-    /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -219,7 +223,7 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
@@ -228,3 +232,4 @@ void assert_failed(uint8_t *file, uint32_t line)
 #endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
