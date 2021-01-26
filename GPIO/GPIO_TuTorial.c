ed05b1f419bb30c5b89394fc8cd22adca922a2c4
SimilarTriangles <36286155+Speike-Zehao@users.noreply.github.com>
2021-01-26 18:58:46 +0800

    Create GPIO_TuTorial.c

---
+++ GPIO/GPIO_TuTorial.c
@@ -0,0 +1,10 @@
+    void Configure_GPIO(){
+    GPIO_InitTypeDef GPIO_Initure;
+    __HAL_RCC_GPIOB_CLK_ENABLE();           
+	
+    GPIO_Initure.Pin=GPIO_PIN_0; //使能PB0 
+    GPIO_Initure.Mode=GPIO_MODE_OUTPUT_PP;  //推挽输出
+    GPIO_Initure.Pull=GPIO_PULLUP;          //上拉
+    GPIO_Initure.Speed=GPIO_SPEED_HIGH;   //GPIO运行模式：高速
+    HAL_GPIO_Init(GPIOB,&GPIO_Initure);  
+    }
